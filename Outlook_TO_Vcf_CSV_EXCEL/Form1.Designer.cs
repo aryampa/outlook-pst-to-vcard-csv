3c20f1716f73335e2b509a773941a81ffb544193
aryampa <aryampa2caleb@yahoo.com>
2018-06-11 22:00:12 +1000

    2014 Files

---
+++ Outlook_TO_Vcf_CSV_EXCEL/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Outlook_TO_Vcf_CSV_EXCEL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.lbl = new System.Windows.Forms.Label();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.lbx1 = new System.Windows.Forms.ListBox();
+            this.rbtnVcf = new System.Windows.Forms.RadioButton();
+            this.RbtnCsv = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.lblProgressText = new System.Windows.Forms.Label();
+            this.tbxFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.Location = new System.Drawing.Point(328, 55);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(165, 40);
+            this.BtnLoad.TabIndex = 0;
+            this.BtnLoad.Text = "Generate Vcf File";
+            this.BtnLoad.UseVisualStyleBackColor = true;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(13, 13);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(484, 23);
+            this.progBar.TabIndex = 2;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(207, 39);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(62, 13);
+            this.lbl.TabIndex = 3;
+            this.lbl.Text = "xxxxxxxxxxx";
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.WorkerReportsProgress = true;
+            // 
+            // lbx1
+            // 
+            this.lbx1.FormattingEnabled = true;
+            this.lbx1.Location = new System.Drawing.Point(26, 182);
+            this.lbx1.Name = "lbx1";
+            this.lbx1.ScrollAlwaysVisible = true;
+            this.lbx1.Size = new System.Drawing.Size(471, 56);
+            this.lbx1.TabIndex = 4;
+            // 
+            // rbtnVcf
+            // 
+            this.rbtnVcf.AutoSize = true;
+            this.rbtnVcf.Location = new System.Drawing.Point(13, 43);
+            this.rbtnVcf.Name = "rbtnVcf";
+            this.rbtnVcf.Size = new System.Drawing.Size(60, 17);
+            this.rbtnVcf.TabIndex = 5;
+            this.rbtnVcf.TabStop = true;
+            this.rbtnVcf.Text = "Vcf File";
+            this.rbtnVcf.UseVisualStyleBackColor = true;
+            // 
+            // RbtnCsv
+            // 
+            this.RbtnCsv.AutoSize = true;
+            this.RbtnCsv.Location = new System.Drawing.Point(13, 67);
+            this.RbtnCsv.Name = "RbtnCsv";
+            this.RbtnCsv.Size = new System.Drawing.Size(46, 17);
+            this.RbtnCsv.TabIndex = 6;
+            this.RbtnCsv.TabStop = true;
+            this.RbtnCsv.Text = "CSV";
+            this.RbtnCsv.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(13, 91);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(59, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "EXCEL";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // lblProgressText
+            // 
+            this.lblProgressText.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblProgressText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgressText.Location = new System.Drawing.Point(226, 148);
+            this.lblProgressText.Name = "lblProgressText";
+            this.lblProgressText.Size = new System.Drawing.Size(267, 23);
+            this.lblProgressText.TabIndex = 9;
+            this.lblProgressText.Text = "--------";
+            this.lblProgressText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbxFileName
+            // 
+            this.tbxFileName.Location = new System.Drawing.Point(26, 156);
+            this.tbxFileName.Name = "tbxFileName";
+            this.tbxFileName.Size = new System.Drawing.Size(168, 20);
+            this.tbxFileName.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "File Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 249);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxFileName);
+            this.Controls.Add(this.lblProgressText);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.RbtnCsv);
+            this.Controls.Add(this.rbtnVcf);
+            this.Controls.Add(this.lbx1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.BtnLoad);
+            this.Name = "Form1";
+            this.Text = "OutLook To Vcf";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.ProgressBar progBar;
+        private System.Windows.Forms.Label lbl;
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.FolderBrowserDialog folderDialog;
+        private System.Windows.Forms.ListBox lbx1;
+        private System.Windows.Forms.RadioButton rbtnVcf;
+        private System.Windows.Forms.RadioButton RbtnCsv;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label lblProgressText;
+        private System.Windows.Forms.TextBox tbxFileName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Outlook_TO_Vcf_CSV_EXCEL/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+using Microsoft.Office.Interop.Outlook;
+
+using outlookObj = Microsoft.Office.Interop.Outlook;
+
+
+namespace Outlook_TO_Vcf_CSV_EXCEL
+{
+    public partial class Form1 : Form
+    {
+        StringBuilder sBuilder;
+        bool imageCreated = false;
+
+
+
+
+        outlookObj.Application outlookApp;
+        outlookObj.NameSpace ns;
+        outlookObj.MAPIFolder contactsFolder;
+        double contactsTotal, processed_Contacts, workPercentage;
+        string out_path;
+
+        string picturepath;
+        //FileStream fs;
+        //StreamWriter sWriter;
+
+        string fullName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                fullName = "";
+                out_path = "";
+                sBuilder = new StringBuilder();
+                outlookApp = new outlookObj.Application();
+                ns = outlookApp.GetNamespace("MAPI");
+                contactsFolder = outlookApp.Session.GetDefaultFolder(OlDefaultFolders.olFolderContacts);
+                contactsTotal = contactsFolder.Items.Count;
+
+                
+
+
+            }
+
+            catch (SystemException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                out_path = folderDialog.SelectedPath;
+               // bgWorker.RunWorkerAsync(); // start background worker do_work routine
+
+
+
+//#####################################################################################################################################
+
+                try
+                {
+                    if (tbxFileName.Text == "")
+                    {
+                        throw new SystemException("File Name Not Provided!!!");
+                    }
+
+                    else
+                    {
+
+                        if (rbtnVcf.Checked) { ///// IF RBTN VCF
+
+
+                           // fs = new FileStream(out_path + "\\"+tbxFileName .Text +".vcf", FileMode.Create, FileAccess.Write);
+                            string fNam = "";
+                            string MNan = "";
+                            string LNam = "";
+
+                           // sWriter = new StreamWriter(fs,Encoding.ASCII,100);
+
+
+                            double total_contatcs = (double)contactsFolder.Items.Count;
+
+                            double work_counter = 1;
+
+                            sBuilder.Clear();
+
+                            foreach (ContactItem contact_record in contactsFolder.Items)
+                            {
+
+                                lblProgressText.Text = work_counter.ToString() + " of " + contactsTotal.ToString();
+                                lblProgressText.Update();
+
+                                if (contact_record.FullName == null)
+                                {
+                                }
+
+                                else
+                                {
+
+                                    fullName = contact_record.FullName.ToString();
+
+
+                                    sBuilder.AppendLine("BEGIN:VCARD");
+                                    sBuilder.AppendLine("VERSION:2.1");
+
+                                    if (contact_record.FirstName != null) fNam = contact_record.FirstName.ToString();
+                                    if (contact_record.MiddleName != null) MNan = contact_record.MiddleName.ToString();
+                                    if (contact_record.LastName != null) LNam = contact_record.LastName.ToString();
+
+
+                                    sBuilder.AppendLine("N:" + fNam + ";" + MNan + ";" + LNam);
+
+                                    sBuilder.AppendLine("FN:" + fNam + " " + MNan + " " + LNam);
+
+
+
+                                   
+
+                                    if (contact_record.CompanyName != null) sBuilder.AppendLine("ORG:" + contact_record.CompanyName.ToString());
+                                    if (contact_record.JobTitle != null) sBuilder.AppendLine("TITLE:" + contact_record.JobTitle.ToString());
+                                    if (contact_record.MobileTelephoneNumber != null) sBuilder.AppendLine("TEL;CELL:" + contact_record.MobileTelephoneNumber.ToString());
+                                    if (contact_record.BusinessTelephoneNumber != null) sBuilder.AppendLine("TEL;WORK:" + contact_record.BusinessTelephoneNumber.ToString());
+                                    if (contact_record.Business2TelephoneNumber != null) sBuilder.AppendLine("TELL;WORK:" + contact_record.Business2TelephoneNumber.ToString());
+                                    if (contact_record.OtherTelephoneNumber != null) sBuilder.AppendLine("TELL;WORK:" + contact_record.OtherTelephoneNumber.ToString());
+
+                                    if (contact_record.HomeTelephoneNumber != null) sBuilder.AppendLine("TELL;HOME:" + contact_record.HomeTelephoneNumber.ToString());
+                                    if (contact_record.Home2TelephoneNumber != null) sBuilder.AppendLine("TELL;HOME:" + contact_record.Home2TelephoneNumber.ToString());
+                                    if (contact_record.Email1Address != null) sBuilder.AppendLine("EMAIL;WORK:" + contact_record.Email1Address.ToString());
+
+                                    if (contact_record.Email2Address != null) sBuilder.AppendLine("EMAIL;HOME:" + contact_record.Email2Address.ToString());
+
+                                    if (contact_record.HasPicture)
+                                    {
+
+                                        foreach (outlookObj.Attachment att in contact_record.Attachments)
+                                        {
+
+                                            if (att.DisplayName.ToString().Contains("ContactPicture"))
+                                            {
+
+
+                                                string picturepath = out_path + "\\" + contact_record.FullName.ToString() + "_" + contact_record.EntryID.ToString() + ".jpg";
+
+                                                if (!File.Exists(picturepath))
+                                                {
+                                                    att.SaveAsFile(picturepath);
+
+                                                    imageCreated = true;
+                                                }
+
+                                                if (imageCreated)
+                                                {
+
+                                                    string base64String_Image = "";
+
+                                                    MemoryStream ms = new MemoryStream();
+
+                                                    Image contactImg = Image.FromFile(picturepath);
+
+                                                    contactImg.Save(ms, ImageFormat.Jpeg);
+
+                                                    byte[] imageBytess = ms.ToArray();
+
+                                                    base64String_Image = Convert.ToBase64String(imageBytess);
+
+                                                    sBuilder.AppendLine("PHOTO;ENCODING=BASE64;JPEG:" + base64String_Image);
+
+                                                    imageCreated = false;
+                                                }
+
+                                            }
+                                        }
+
+                                    }
+
+                                    sBuilder.AppendLine("END:VCARD");
+
+
+
+                                    processed_Contacts = work_counter;
+                                    work_counter++;
+                                }
+                            }
+
+                            
+                           // sWriter.Write(sBuilder.ToString());
+
+                          
+                           //sWriter.Write("This is test test");
+                           // fs.Flush();
+
+                            TextWriter tw = File.CreateText(out_path + "\\" + tbxFileName.Text + ".vcf");
+                            tw.Write(sBuilder.ToString());
+
+                            tw.Close();
+
+                            MessageBox.Show("Vcard Successfully Created!!!");
+
+                            lblProgressText.Text = "";
+
+
+
+                        }
+
+                        else if (RbtnCsv.Checked) { /////// IF RBTN CSV
+
+
+                        }
+                    }
+                }
+
+                catch (SystemException ex)
+                {
+                    MessageBox.Show(ex.Message,"Error");
+                    
+                }
+
+
+//#####################################################################################################################################
+
+            }
+
+            else MessageBox.Show("Output Directory Selection Canceled");
+
+
+
+
+
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Process[] running_Processes = Process.GetProcesses();
+                foreach (Process active_process in running_Processes)
+                {
+                    if (active_process.ProcessName.ToUpper() == "OUTLOOK.EXE")
+                    {
+                        MessageBox.Show(active_process.ProcessName.ToUpper());
+                        active_process.Close();
+                    }
+                }
+            }
+
+            catch (System.Exception ex) {
+                MessageBox.Show("Error Closing OUTLOOK!! " + "ERROR: " + ex.Message.ToString());
+                this.Close();
+
+            }
+
+        }
+    }
+}
